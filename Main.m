435f142c73377585aa7f69ea17953cc6d5f734cf
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-03 23:08:18 +0300

    Cleaned Transmission Ratio Search

--- Main.m
+++ Main.m
@@ -12,33 +12,8 @@ Vt = fzero(@(V) vehicle.Acceleration(V), 100*air.V);
 [Vmin, Vmax] = vehicle.SpeedBoundary();
 fprintf('r=1 Vt=%.1fm/s B=[%.1f, %.1f]m/s\n', Vt, Vmin, Vmax);
 
-rr = 0.5:0.01:3;
-
-[Vt, rt] = TransmissionRatioSearch(vehicle, rr, 1);
-vehicle = vehicle.Setr(rt);
-[Vmin, Vmax] = vehicle.SpeedBoundary();
-fprintf('Best Bounded r=%.2f Vt=%.1fm/s B=[%.1f, %.1f]m/s\n', vehicle.r, Vt, Vmin, Vmax);
-title('Bounded Top Speed vs Transmission Ratio');
-
-[Vt, rt] = TransmissionRatioSearch(vehicle, rr, 10);
-vehicle = vehicle.Setr(rt);
-[Vmin, Vmax] = vehicle.SpeedBoundary();
-fprintf('Best Unbounded r=%.2f Vt=%.1fm/s B=[%.1f, %.1f]m/s\n', vehicle.r, Vt, Vmin, Vmax);
-title('Unbounded Top Speed vs Transmission Ratio');
-
-function [Vt, rt] = TransmissionRatioSearch(vehicle, rr, boundMultiplier)
-	Vr = zeros(size(rr));
-	for k = 1:length(rr)
-		vehicle = vehicle.Setr(rr(k));
-		[Vmin, Vmax] = vehicle.SpeedBoundary();
-		Vr(k) = BisectionMethod(@(V) vehicle.Acceleration(V), Vmin, Vmax*boundMultiplier, 1e-6, 1e3);
-	end
-	[Vt, kt] = max(Vr);
-	rt = rr(kt);
-	figure();
-	hold('on');
-	grid('on');
-	xlabel('Transmission Ratio');
-	ylabel('Top Speed (m/s)');
-	plot(rr, Vr, 'LineWidth', 2);
-end
+[Vt, rt, Vb, rb] = SearchTransmissionRatios(Vehicle(fan, air, 0.2, 0.35, 1), 0.5:0.01:3);
+[Vminb, Vmaxb] = vehicle.Setr(rb).SpeedBoundary();
+[Vmint, Vmaxt] = vehicle.Setr(rt).SpeedBoundary();
+fprintf('Best Bounded  r=%.2f Vt=%.1fm/s B=[%.1f, %.1f]m/s\n', rb, Vb, Vminb, Vmaxb);
+fprintf('Best Ubounded r=%.2f Vt=%.1fm/s B=[%.1f, %.1f]m/s\n', rt, Vt, Vmint, Vmaxt);
---
+++ SearchTransmissionRatios.m
@@ -0,0 +1,33 @@
+function [Vt, rt, Vb, rb] = SearchTransmissionRatios(vehicle, rr)
+	Vminr = zeros(size(rr));
+	Vmaxr = zeros(size(rr));
+	Vr = zeros(size(rr));
+	Vb = -inf;
+	for k = 1:length(rr)
+		vehicle = vehicle.Setr(rr(k));
+		[Vmin, Vmax] = vehicle.SpeedBoundary();
+		Vminr(k) = Vmin;
+		Vmaxr(k) = Vmax;
+		Vr(k) = BisectionMethod(@(V) vehicle.Acceleration(V), Vmin, Vmax*100, 1e-6, 1e3);
+		if Vr(k) <= Vmax
+			if Vr(k) > Vb
+				Vb = Vr(k);
+				rb = vehicle.r;
+			end
+		end
+	end
+	[Vt, kt] = max(Vr);
+	rt = rr(kt);
+	figure();
+	hold('on');
+	grid('on');
+	title('Top Speed vs Transmission Ratio');
+	xlabel('r');
+	ylabel('v_{top} (m/s)');
+	plot(rr, Vminr, 'LineWidth', 2);
+	plot(rr, Vmaxr, 'LineWidth', 2);
+	plot(rr, Vr, 'LineWidth', 2);
+	plot(rb, Vb, 'x', 'MarkerSize', 12, 'LineWidth', 2);
+	plot(rt, Vt, 'x', 'MarkerSize', 12, 'LineWidth', 2);
+	legend('Lower Speed Limit', 'Upper Speed Limit', 'Top Speed', 'Bounded Best', 'Best', 'Location', 'Best');
+end
