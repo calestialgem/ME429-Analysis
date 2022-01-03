2a3f48b99d82d1b311a15342e9be6e2d1a45fbd4
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-03 18:00:50 +0300

    Created Fit

--- Fan.m
+++ Fan.m
@@ -13,6 +13,25 @@ classdef Fan
 			self.w_data = w_data .* (2*pi/60);
 			self.Ct_data = Ct_data ./ (2*pi)^2;
 			self.Cq_data = Cp_data ./ (2*pi)^4;
+			N_J = length(self.J_data);
+			N_w = length(self.w_data);
+			N = N_J*N_w;
+			J_all = zeros(N);
+			w_all = zeros(N);
+			Ct_all = zeros(N);
+			Cq_all = zeros(N);
+			for k = 1:N_w
+				last = k*N_J;
+				first = last - N_J + 1;
+				elements = last:first;
+				J_all(elements) = self.J_data;
+				w_all(elements) = self.w_data(k);
+				Ct_all(elements) = self.Ct_data(k, :);
+				Cq_all(elements) = self.Cq_data(k, :);
+			end
+			points = [J_all, w_all];
+			Ct = fit(points, Ct_all, 'poly55', 'Normalize', 'on');
+			Cq = fit(points, Cq_all, 'poly55', 'Normalize', 'on');
 		end
 		function [T, Q] = Find(self, rho, J, w)
 		end
