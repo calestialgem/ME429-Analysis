bb6ed28c20f07314ded977d9f00cc91b360ca28d
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-03 18:18:58 +0300

    Fixed Fan Fitting

--- Fan.m
+++ Fan.m
@@ -16,27 +16,27 @@ classdef Fan
 			N_J = length(self.J_data);
 			N_w = length(self.w_data);
 			N = N_J*N_w;
-			J_all = zeros(N);
-			w_all = zeros(N);
-			Ct_all = zeros(N);
-			Cq_all = zeros(N);
+			J_all = zeros(N, 1);
+			w_all = zeros(N, 1);
+			Ct_all = zeros(N, 1);
+			Cq_all = zeros(N, 1);
 			for k = 1:N_w
 				last = k*N_J;
 				first = last - N_J + 1;
-				elements = last:first;
+				elements = first:last;
 				J_all(elements) = self.J_data;
 				w_all(elements) = self.w_data(k);
 				Ct_all(elements) = self.Ct_data(k, :);
 				Cq_all(elements) = self.Cq_data(k, :);
 			end
 			points = [J_all, w_all];
-			Ct = fit(points, Ct_all, 'poly55', 'Normalize', 'on');
-			Cq = fit(points, Cq_all, 'poly55', 'Normalize', 'on');
+			self.Ct = fit(points, Ct_all, 'poly55', 'Normalize', 'on');
+			self.Cq = fit(points, Cq_all, 'poly55', 'Normalize', 'on');
 		end
 		function [T, Q] = Find(self, rho, D, V, w)
 			J = V/(w*D);
-			T = rho*w^2*D^4*Ct(J, w);
-			Q = rho*w^2*D^5*Cq(J, w);
+			T = rho*w^2*D^4*self.Ct(J, w);
+			Q = rho*w^2*D^5*self.Cq(J, w);
 		end
 	end
 end
