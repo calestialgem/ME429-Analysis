dea6115fcc0e147152755f1c33a599ee396a3e0d
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-03 22:03:43 +0300

    Searching the Transmission Ratio Space

--- Main.m
+++ Main.m
@@ -10,11 +10,17 @@ air = Air(5, 1.225);
 vehicle = Vehicle(fan, air, 0.2, 0.35, 1);
 Vm = fzero(@(V) vehicle.Acceleration(V), 100*air.V);
 fprintf('Found Top Speed: %g', Vm);
+
+rr = 0.5:0.1:3;
+Vr = zeros(size(rr));
+for k = 1:length(rr)
+	vehicle = vehicle.Setr(rr(k));
+	Vr(k) = fzero(@(V) vehicle.Acceleration(V), 100*air.V);
+end
 figure();
 hold('on');
 grid('on');
-xlabel('Speed (m/s)');
-ylabel('Acceleration (m/s^2)');
-title('Acceleration vs Speed');
-Vr = air.V:air.V*0.1:air.V*11.75;
-plot(Vr, vehicle.Acceleration(Vr), 'LineWidth', 2);
+xlabel('Transmission Ratio');
+ylabel('Top Speed (m/s)');
+title('Top Speed vs Transmission Ratio');
+plot(rr, Vr, 'LineWidth', 2);
