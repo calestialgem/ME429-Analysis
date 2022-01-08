015d90c627148fe3601117c4140fbf0e7eec6d5a
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-08 17:10:21 +0300

    Added Wheel Bearing Friction

--- Main.m
+++ Main.m
@@ -13,7 +13,7 @@ end
 
 fan = GetAPC14x7E();
 air = Air(5, 1.225);
-vehicle = Vehicle(fan, air, 0.2, 0.35, 1.52);
+vehicle = Vehicle(fan, air, 350e-3, 0.35, 1.52);
 v = Simulate(vehicle, 20);
 [Vmin, Vmax] = vehicle.SpeedBoundary();
 fprintf(fileID, 'r=%.2f Vt=%.1fm/s B=[%.1f, %.1f]m/s\n', vehicle.r, max(v), Vmin, Vmax);
--- Vehicle.m
+++ Vehicle.m
@@ -2,10 +2,14 @@ classdef Vehicle
 	properties
 		fan Fan
 		air Air
-		m
 		L
 		Dw
+		Mw
 		Iw
+		m
+		P
+		f
+		b
 		r
 		i
 		s
@@ -14,10 +18,14 @@ classdef Vehicle
 		function self = Vehicle(fan, air, m, L, r)
 			self.fan = fan;
 			self.air = air;
-			self.m = m + fan.m;
-			self.L = L;
 			self.Dw = 0.15*L;
-			self.Iw = 0;
+			self.Mw = (self.Dw^2-8.35e-3^2)*pi/4*5e-3*0.6e3;
+			self.Iw = self.Mw*self.Dw^2/8;
+			self.m = m + fan.m + self.Mw;
+			self.P = ((m+fan.m)/2)/(2*(6.35e-3/2)*5e-3);
+			self.f = 2*pi^2*0.35/self.P*(6.35e-3/2)/1e-3;
+			self.b = 2*(6.35e-3/2)^2*self.f*5e-3*self.P*2/(2*pi)*60;
+			self.L = L;
 			self.r = r;
 			self = self.Findi();
 			self = self.Finds();
@@ -33,11 +41,13 @@ classdef Vehicle
 			self = self.Findi();
 			self = self.Finds();
 		end
-		function [a, Vt, D, T, Q, F] = Acceleration(self, V)
+		function [a, Vt, w, D, T, Q, B, F] = Acceleration(self, V)
 			Vt = self.air.TrueSpeed(V);
 			D = -0.003.*Vt.^2;
-			[T, Q] = self.fan.Find(self.air.R, Vt, self.s.*V);
-			F = D+T-self.s.*Q;
+			w = self.s.*V;
+			B = self.b.*w;
+			[T, Q] = self.fan.Find(self.air.R, Vt, w);
+			F = D+T-self.s.*Q-(2/self.Dw).*B;
 			a = F/self.i;
 		end
 		function [Vmin, Vmax] = SpeedBoundary(self)
