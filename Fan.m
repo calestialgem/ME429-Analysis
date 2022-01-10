0f598a319d41bab94beb2d3f4768b61939207f33
Cem Geçgel <gecgelcem@outlook.com>
2022-01-10 22:04:41 +0300

    Started Changing Coefficiencts

--- Fan.m
+++ Fan.m
@@ -6,49 +6,52 @@ classdef Fan
 		I
 		J_data
 		w_data
-		Ct_data
-		Cq_data
-		Ct
-		Cq
-		Ct_rms
-		Cq_rms
+		T_data
+		Q_data
+		T
+		Q
+		T_rms
+		Q_rms
 	end
 	methods
-		function self = Fan(name, m, D, J_data, w_data, Ct_data, Cp_data)
+		function self = Fan(name, m, D, J_data, w_data, T_data, Q_data)
 			self.Name = name;
 			self.m = m*28.3495231e-3;
 			self.D = D*2.54e-2;
 			self.I = self.m*self.D^2/12;
 			self.J_data = J_data ./ (2*pi);
 			self.w_data = w_data .* (2*pi/60);
-			self.Ct_data = Ct_data ./ (2*pi)^2;
-			self.Cq_data = Cp_data ./ (2*pi)^4;
+			self.T_data = T_data ./ (2*pi)^2;
+			self.Q_data = Q_data ./ (2*pi)^4;
 			N_J = length(self.J_data);
 			N_w = length(self.w_data);
 			N = N_J*N_w;
 			J_all = zeros(N, 1);
 			w_all = zeros(N, 1);
-			Ct_all = zeros(N, 1);
-			Cq_all = zeros(N, 1);
+			T_all = zeros(N, 1);
+			Q_all = zeros(N, 1);
 			for k = 1:N_w
 				last = k*N_J;
 				first = last - N_J + 1;
 				elements = first:last;
 				J_all(elements) = self.J_data;
 				w_all(elements) = self.w_data(k);
-				Ct_all(elements) = self.Ct_data(k, :);
-				Cq_all(elements) = self.Cq_data(k, :);
+				T_all(elements) = self.T_data(k, :);
+				Q_all(elements) = self.Q_data(k, :);
 			end
 			points = [J_all, w_all];
-			self.Ct = fit(points, Ct_all, 'poly55', 'Normalize', 'on');
-			self.Cq = fit(points, Cq_all, 'poly55', 'Normalize', 'on');
-			self.Ct_rms = sqrt(sum((self.Ct(points)-Ct_all).^2))/N/(max(Ct_all)-min(Ct_all));
-			self.Cq_rms = sqrt(sum((self.Cq(points)-Cq_all).^2))/N/(max(Cq_all)-min(Cq_all));
+			self.T = fit(points, T_all, 'poly55', 'Normalize', 'on');
+			self.Q = fit(points, Q_all, 'poly55', 'Normalize', 'on');
+			self.T_rms = sqrt(sum((self.T(points)-T_all).^2))/N/(max(T_all)-min(T_all));
+			self.Q_rms = sqrt(sum((self.Q(points)-Q_all).^2))/N/(max(Q_all)-min(Q_all));
 		end
 		function [T, Q] = Find(self, R, V, w)
 			J = V./(w.*self.D);
-			T = R.*w.^2.*self.D^4.*self.Ct(J, w);
-			Q = R.*w.^2.*self.D^5.*self.Cq(J, w);
+			T = R.*w.^2.*self.D^4.*self.T(J, w);
+			Q = R.*w.^2.*self.D^5.*self.Q(J, w);
+		end
+		function RMS = RootMeanSquare(self)
+			RMS = sprintf('Root Mean Squares T=%e Q=%e\n', self.T_rms, self.Q_rms);
 		end
 		function PlotFitJ(self)
 			J_range = 0:self.J_data(end)/100:self.J_data(end);
@@ -59,12 +62,12 @@ classdef Fan
 			grid('on');
 			xlabel('J');
 			ylabel('C_t');
-			plot(J_range, self.Ct(J_range, self.w_data(1)), '-r', 'LineWidth', 2);
-			plot(J_range, self.Ct(J_range, self.w_data(w_middle)), '-g', 'LineWidth', 2);
-			plot(J_range, self.Ct(J_range, self.w_data(end)), '-b', 'LineWidth', 2);
-			plot(self.J_data, self.Ct_data(1, :), 'xr', 'LineWidth', 2);
-			plot(self.J_data, self.Ct_data(w_middle, :), 'xg', 'LineWidth', 2);
-			plot(self.J_data, self.Ct_data(end, :), 'xb', 'LineWidth', 2);
+			plot(J_range, self.T(J_range, self.w_data(1)), '-r', 'LineWidth', 2);
+			plot(J_range, self.T(J_range, self.w_data(w_middle)), '-g', 'LineWidth', 2);
+			plot(J_range, self.T(J_range, self.w_data(end)), '-b', 'LineWidth', 2);
+			plot(self.J_data, self.T_data(1, :), 'xr', 'LineWidth', 2);
+			plot(self.J_data, self.T_data(w_middle, :), 'xg', 'LineWidth', 2);
+			plot(self.J_data, self.T_data(end, :), 'xb', 'LineWidth', 2);
 			legend(...
 				sprintf('Fit w=%.0frad/s', self.w_data(1)),...
 				sprintf('Fit w=%.0frad/s', self.w_data(w_middle)),...
@@ -78,12 +81,12 @@ classdef Fan
 			grid('on');
 			xlabel('J');
 			ylabel('C_q');
-			plot(J_range, self.Cq(J_range, self.w_data(1)), '-r', 'LineWidth', 2);
-			plot(J_range, self.Cq(J_range, self.w_data(w_middle)), '-g', 'LineWidth', 2);
-			plot(J_range, self.Cq(J_range, self.w_data(end)), '-b', 'LineWidth', 2);
-			plot(self.J_data, self.Cq_data(1, :), 'xr', 'LineWidth', 2);
-			plot(self.J_data, self.Cq_data(w_middle, :), 'xg', 'LineWidth', 2);
-			plot(self.J_data, self.Cq_data(end, :), 'xb', 'LineWidth', 2);
+			plot(J_range, self.Q(J_range, self.w_data(1)), '-r', 'LineWidth', 2);
+			plot(J_range, self.Q(J_range, self.w_data(w_middle)), '-g', 'LineWidth', 2);
+			plot(J_range, self.Q(J_range, self.w_data(end)), '-b', 'LineWidth', 2);
+			plot(self.J_data, self.Q_data(1, :), 'xr', 'LineWidth', 2);
+			plot(self.J_data, self.Q_data(w_middle, :), 'xg', 'LineWidth', 2);
+			plot(self.J_data, self.Q_data(end, :), 'xb', 'LineWidth', 2);
 			legend(...
 				sprintf('Fit w=%.0frad/s', self.w_data(1)),...
 				sprintf('Fit w=%.0frad/s', self.w_data(w_middle)),...
@@ -101,12 +104,12 @@ classdef Fan
 			grid('on');
 			xlabel('w (rad/s)');
 			ylabel('C_t');
-			plot(w_range, self.Ct(self.J_data(1), w_range), '-r', 'LineWidth', 2);
-			plot(w_range, self.Ct(self.J_data(J_middle), w_range), '-g', 'LineWidth', 2);
-			plot(w_range, self.Ct(self.J_data(end), w_range), '-b', 'LineWidth', 2);
-			plot(self.w_data, self.Ct_data(:, 1), 'xr', 'LineWidth', 2);
-			plot(self.w_data, self.Ct_data(:, J_middle), 'xg', 'LineWidth', 2);
-			plot(self.w_data, self.Ct_data(:, end), 'xb', 'LineWidth', 2);
+			plot(w_range, self.T(self.J_data(1), w_range), '-r', 'LineWidth', 2);
+			plot(w_range, self.T(self.J_data(J_middle), w_range), '-g', 'LineWidth', 2);
+			plot(w_range, self.T(self.J_data(end), w_range), '-b', 'LineWidth', 2);
+			plot(self.w_data, self.T_data(:, 1), 'xr', 'LineWidth', 2);
+			plot(self.w_data, self.T_data(:, J_middle), 'xg', 'LineWidth', 2);
+			plot(self.w_data, self.T_data(:, end), 'xb', 'LineWidth', 2);
 			legend(...
 				sprintf('Fit J=%.2f', self.J_data(1)),...
 				sprintf('Fit J=%.2f', self.J_data(J_middle)),...
@@ -120,12 +123,12 @@ classdef Fan
 			grid('on');
 			xlabel('w (rad/s)');
 			ylabel('C_q');
-			plot(w_range, self.Cq(self.J_data(1), w_range), '-r', 'LineWidth', 2);
-			plot(w_range, self.Cq(self.J_data(J_middle), w_range), '-g', 'LineWidth', 2);
-			plot(w_range, self.Cq(self.J_data(end), w_range), '-b', 'LineWidth', 2);
-			plot(self.w_data, self.Cq_data(:, 1), 'xr', 'LineWidth', 2);
-			plot(self.w_data, self.Cq_data(:, J_middle), 'xg', 'LineWidth', 2);
-			plot(self.w_data, self.Cq_data(:, end), 'xb', 'LineWidth', 2);
+			plot(w_range, self.Q(self.J_data(1), w_range), '-r', 'LineWidth', 2);
+			plot(w_range, self.Q(self.J_data(J_middle), w_range), '-g', 'LineWidth', 2);
+			plot(w_range, self.Q(self.J_data(end), w_range), '-b', 'LineWidth', 2);
+			plot(self.w_data, self.Q_data(:, 1), 'xr', 'LineWidth', 2);
+			plot(self.w_data, self.Q_data(:, J_middle), 'xg', 'LineWidth', 2);
+			plot(self.w_data, self.Q_data(:, end), 'xb', 'LineWidth', 2);
 			legend(...
 				sprintf('Fit J=%.2f', self.J_data(1)),...
 				sprintf('Fit J=%.2f', self.J_data(J_middle)),...
--- Main.m
+++ Main.m
@@ -12,7 +12,7 @@ if ~isempty(errmsg)
 end
 
 fan = GetAPC14x7E();
-fprintf(fileID, 'Ct_rms = %e Cq_rms = %e\n', fan.Ct_rms, fan.Cq_rms);
+fprintf(fileID, fan.RootMeanSquare());
 air = Air(5, 1.225);
 
 vehicle = Vehicle(fan, air, 1);
