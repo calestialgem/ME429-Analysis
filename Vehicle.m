7adf6de3492faca9f34ea027354688877e47a9f2
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 21:30:03 +0300

    Fixed Bearing Friction Calculation

--- Vehicle.m
+++ Vehicle.m
@@ -20,20 +20,21 @@ classdef Vehicle
 			I_wheel = m_wheel*d^2/8;
 			m_body = 350e-3;
 			m = m_body+fan.m+m_wheel*3;
+			u = 0.4;
 			k_r = 6.35e-3/2;
 			k_c = 1e-3;
-			k_l = 5e-3;
-			k_u = 0.35;
-			self.mul_k = 4*pi*k_u*k_r^3*k_l/k_c/d;
+			k_l = 10e-3;
+			self.mul_k = 4*pi*u*k_r^3*k_l/k_c/d;
 			self.mul_a = (m+(12*I_wheel+4*self.fan.I/self.Z^2)/d^2)^-1;
 			self.mul_q = 2/(self.Z*d);
 		end
 		function [a, v_t, F_t, F_q, F_k, F_net] = Acceleration(self, v)
 			v_t = self.air.TrueSpeed(v);
-			w = self.mul_q*v;
-			[F_t, T_q] = self.fan.Find(self.air, v_t, w);
+			w_c = self.mul_q*v;
+			w_k = self.Z*w_c;
+			[F_t, T_q] = self.fan.Find(self.air, v_t, w_c);
 			F_q = self.mul_q*T_q;
-			F_k = self.mul_k*w;
+			F_k = self.mul_k*w_k;
 			F_net = F_t-F_q-F_k;
 			a = F_net*self.mul_a;
 		end
