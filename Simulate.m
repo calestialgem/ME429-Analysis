ece7c45d614b28c37c0126900295c3b6d958131e
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 13:28:31 +0300

    Refactored Simulation

--- Simulate.m
+++ Simulate.m
@@ -1,28 +1,28 @@
-function v = Simulate(vehicle, Tf)
-	[Vmin, Vmax] = vehicle.SpeedBoundary();
-	[t, v] = RungeKuttaMethod(@(~, V) vehicle.Acceleration(V), 1e3, Tf, Vmin);
-	[A, Vt, T, Q, B, F] = vehicle.Acceleration(v);
+function v_range = Simulate(vehicle, Tf)
+	[v_min, ~] = vehicle.SpeedBoundary();
+	[time, v_range] = RungeKuttaMethod(@(~, V) vehicle.Acceleration(V), 1e3, Tf, v_min);
+	[a_range, ~, T_range, Q_range, B_range, F_range] = vehicle.Acceleration(v_range);
 	figure();
 	hold('on');
 	grid('on');
-	plot(t, v, 'LineWidth', 2);
+	plot(time, v_range, 'LineWidth', 2);
 	xlabel('Time (s)');
 	ylabel('Velocity (m/s)');
 	title(sprintf('Speed Diagram t=%.2f', vehicle.t));
 	figure();
 	hold('on');
 	grid('on');
-	plot(t, A, 'LineWidth', 2);
+	plot(time, a_range, 'LineWidth', 2);
 	xlabel('Time (s)');
 	ylabel('Acceleration (m/s^2)');
 	title(sprintf('Acceleration Diagram t=%.2f', vehicle.t));
 	figure();
 	hold('on');
 	grid('on');
-	plot(t, T, 'LineWidth', 2);
-	plot(t, Q, 'LineWidth', 2);
-	plot(t, B, 'LineWidth', 2);
-	plot(t, F, 'LineWidth', 2);
+	plot(time, T_range, 'LineWidth', 2);
+	plot(time, Q_range, 'LineWidth', 2);
+	plot(time, B_range, 'LineWidth', 2);
+	plot(time, F_range, 'LineWidth', 2);
 	xlabel('Time (s)');
 	ylabel('Force (N)');
 	title(sprintf('Force Diagram t=%.2f', vehicle.t));
