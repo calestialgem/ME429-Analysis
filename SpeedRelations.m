df9aafa6cbec1d10e6b99b74d5fb1a49da9db57a
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 17:46:36 +0300

    Revert "Removed Bearing Friction"
    This reverts commit ae924dcc1006b3bb80a95b944e7da596bda7233a.

--- SpeedRelations.m
+++ SpeedRelations.m
@@ -1,7 +1,7 @@
 function v_range = SpeedRelations(vehicle)
 	[v_min, v_max] = vehicle.SpeedBoundary();
 	v_range = v_min:(v_max-v_min)/1e3:v_max;
-	[a_range, ~, T_range, Q_range, F_range] = vehicle.Acceleration(v_range);
+	[a_range, ~, T_range, Q_range, B_range, F_range] = vehicle.Acceleration(v_range);
 	[~, k_top] = min(abs(a_range));
 	v_bounds = [v_min, v_range(k_top)];
 	figure();
@@ -17,10 +17,11 @@ function v_range = SpeedRelations(vehicle)
 	grid('on');
 	plot(v_range, T_range, 'LineWidth', 2);
 	plot(v_range, Q_range, 'LineWidth', 2);
+	plot(v_range, B_range, 'LineWidth', 2);
 	plot(v_range, F_range, 'LineWidth', 2);
 	xlabel('Velocity (m/s)');
 	xlim(v_bounds);
 	ylabel('Force (N)');
 	title(sprintf('Force vs Speed SR=%.2f', vehicle.SR));
-	legend('Thrust', 'Torque', 'Net', 'Location', 'Best');
+	legend('Thrust', 'Torque', 'Bearing Friction', 'Net', 'Location', 'Best');
 end
