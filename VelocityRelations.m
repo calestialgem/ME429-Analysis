960b4135bd32ace70cd8b94c9f8a90ba95bf7a74
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 20:08:16 +0300

    Changed Figure Bounds vs Velocity

--- VelocityRelations.m
+++ VelocityRelations.m
@@ -3,25 +3,26 @@ function v_range = VelocityRelations(vehicle)
 	v_range = v_min:(v_max-v_min)/1e3:v_max;
 	[a_range, ~, F_t_range, F_q_range, F_k_range, F_net_range] = vehicle.Acceleration(v_range);
 	[~, k_top] = min(abs(a_range));
-	v_bounds = [v_min, v_range(k_top)];
+	if a_range(k_top) < 0
+		k_top = k_top - 1;
+	end
+	k_range = 1:k_top;
 	figure();
 	hold('on');
 	grid('on');
-	plot(v_range, a_range, 'LineWidth', 2);
-	xlabel('v (m/s)');
-	xlim(v_bounds);
+	plot(v_range(k_range), a_range(k_range), 'LineWidth', 2);
 	ylabel('a (m/s^2)');
+	xlabel('v (m/s)');
 	title(sprintf('Acceleration vs Velocity Z=%.2f', vehicle.Z));
 	figure();
 	hold('on');
 	grid('on');
-	plot(v_range, F_t_range, 'LineWidth', 2);
-	plot(v_range, F_q_range, 'LineWidth', 2);
-	plot(v_range, F_k_range, 'LineWidth', 2);
-	plot(v_range, F_net_range, 'LineWidth', 2);
-	xlabel('v (m/s)');
-	xlim(v_bounds);
+	plot(v_range(k_range), F_t_range(k_range), 'LineWidth', 2);
+	plot(v_range(k_range), F_q_range(k_range), 'LineWidth', 2);
+	plot(v_range(k_range), F_k_range(k_range), 'LineWidth', 2);
+	plot(v_range(k_range), F_net_range(k_range), 'LineWidth', 2);
 	ylabel('F (N)');
+	xlabel('v (m/s)');
 	title(sprintf('Forces vs Velocity Z=%.2f', vehicle.Z));
 	legend('F_t', 'F_q', 'F_k', 'F_{net}', 'Location', 'Best');
 end
