e1d4ba17ea9001eb5ecea551521e2d528d06128a
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 21:49:24 +0300

    Fan Shaft Friction

--- Simulate.m
+++ Simulate.m
@@ -1,7 +1,7 @@
 function v_range = Simulate(vehicle, t_f)
 	[v_min, ~] = vehicle.SpeedBoundary();
 	[t_range, v_range] = RungeKuttaMethod(@(~, V) vehicle.Acceleration(V), 1e3, t_f, v_min);
-	[a_range, ~, F_t_range, F_q_range, F_k_range, F_net_range] = vehicle.Acceleration(v_range);
+	[a_range, ~, F_t_range, F_q_range, F_c_range, F_k_range, F_net_range] = vehicle.Acceleration(v_range);
 	figure();
 	hold('on');
 	grid('on');
@@ -23,11 +23,12 @@ function v_range = Simulate(vehicle, t_f)
 	grid('on');
 	plot(t_range, F_t_range, 'LineWidth', 2);
 	plot(t_range, F_q_range, 'LineWidth', 2);
+	plot(t_range, F_c_range, 'LineWidth', 2);
 	plot(t_range, F_k_range, 'LineWidth', 2);
 	plot(t_range, F_net_range, 'LineWidth', 2);
 	xlabel('t (s)');
 	ylabel('F (N)');
 	title(sprintf('Forces vs Time Z=%.2f', vehicle.Z));
-	legend('F_t', 'F_q', 'F_k', 'F_{net}', 'Location', 'Best');
+	legend('F_t', 'F_q', 'F_c', 'F_k', 'F_{net}', 'Location', 'Best');
 	saveas(gcf, 'Forces vs Time', 'jpeg');
 end
--- Vehicle.m
+++ Vehicle.m
@@ -3,9 +3,10 @@ classdef Vehicle
 		air Air
 		fan Fan
 		Z
-		mul_k
 		mul_a
 		mul_q
+		mul_k
+		mul_c
 	end
 	methods
 		function self = Vehicle(air, fan, Z)
@@ -20,22 +21,28 @@ classdef Vehicle
 			I_wheel = m_wheel*d^2/8;
 			m_body = 350e-3;
 			m = m_body+fan.m+m_wheel*3;
+			self.mul_a = (m+(12*I_wheel+4*self.fan.I/self.Z^2)/d^2)^-1;
+			self.mul_q = 2/(self.Z*d);
 			u = 0.4;
+			mul_u = 2*pi*u*self.mul_q;
+			c_r = 6.35e-3/2;
+			c_c = 1e-3;
+			c_l = 87.5e-3;
+			self.mul_c = mul_u*c_r^3*c_l/c_c;
 			k_r = 6.35e-3/2;
 			k_c = 1e-3;
 			k_l = 10e-3;
-			self.mul_k = 4*pi*u*k_r^3*k_l/k_c/d;
-			self.mul_a = (m+(12*I_wheel+4*self.fan.I/self.Z^2)/d^2)^-1;
-			self.mul_q = 2/(self.Z*d);
+			self.mul_k = self.Z*mul_u*k_r^3*k_l/k_c;
 		end
-		function [a, v_t, F_t, F_q, F_k, F_net] = Acceleration(self, v)
+		function [a, v_t, F_t, F_q, F_c, F_k, F_net] = Acceleration(self, v)
 			v_t = self.air.TrueSpeed(v);
 			w_c = self.mul_q*v;
 			w_k = self.Z*w_c;
 			[F_t, T_q] = self.fan.Find(self.air, v_t, w_c);
 			F_q = self.mul_q*T_q;
+			F_c = self.mul_c*w_c;
 			F_k = self.mul_k*w_k;
-			F_net = F_t-F_q-F_k;
+			F_net = F_t-F_q-F_c-F_k;
 			a = F_net*self.mul_a;
 		end
 		function [v_min, v_max] = SpeedBoundary(self)
