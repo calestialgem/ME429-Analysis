cafa58080ca2d0fc89c502d6342f411b4d4ed97c
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 14:43:46 +0300

    Removed NaN Values

--- BestTransmissionRatio.m
+++ BestTransmissionRatio.m
@@ -1,4 +1,8 @@
 function [t_best, v_top] = BestTransmissionRatio(air, fan, t_min, t_max)
 	[t_best, v_top] = fminbnd(@(t) -TopSpeed(Vehicle(air, fan, t)), t_min, t_max);
-	v_top = -v_top;
+	if isnan(v_top)
+		t_best = NaN;
+	else
+		v_top = -v_top;
+	end
 end
