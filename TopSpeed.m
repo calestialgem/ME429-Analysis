f1234bda3e3b61c3562da2594374062fe076cb22
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-22 14:39:29 +0300

    Extrapolate 1e6 times Above

--- TopSpeed.m
+++ TopSpeed.m
@@ -1,4 +1,4 @@
 function v_top = TopSpeed(vehicle)
 	[v_min, v_max] = vehicle.SpeedBoundary();
-	v_top = BisectionMethod(@(v) vehicle.Acceleration(v), v_min, v_max*1000, 1e-6, 1e3);
+	v_top = BisectionMethod(@(v) vehicle.Acceleration(v), v_min, v_max*1e6, 1e-6, 1e3);
 end
