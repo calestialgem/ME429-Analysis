2508675d3161dd9e294371a18df3db85ea035d73
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-11 10:19:22 +0300

    Removed Friction

--- Vehicle.m
+++ Vehicle.m
@@ -16,24 +16,20 @@ classdef Vehicle
             self.d = d;
             self.Z = 1;
             self.F_d = F_d;
-            % From the experiments.
             m = 300e-3;
             % https://www.grainger.com/know-how/equipment-information/kh-types-of-belt-drives-efficiency
             n = 0.9;
-            % From the experiments m_wheel = 0.
             self.mul_a = (m + 4 * self.fan.I / (n * self.Z^2) / self.d^2)^ - 1;
             self.mul_q = 2 / (n * self.Z * self.d);
         end
-        function [a, v_t, F_t, F_d, F_q, F_f, F_net] = Acceleration(self, v)
+        function [a, v_t, F_t, F_d, F_q, F_net] = Acceleration(self, v)
             v_t = self.air.TrueSpeed(v);
             w_b = 2 / self.d * v;
             w_p = w_b / self.Z;
             [F_t, T_q] = self.fan.Find(self.air, v_t, w_p);
             F_d = self.F_d(v_t);
             F_q = self.mul_q * T_q;
-            % From the experiments.
-            F_f = 0.04905 + v / (20/3600e-3) * (0.0614214);
-            F_net = F_t - F_q - F_f - F_d;
+            F_net = F_t - F_d - F_q;
             a = F_net * self.mul_a;
         end
         function [v_min, v_max] = SpeedBoundary(self)
