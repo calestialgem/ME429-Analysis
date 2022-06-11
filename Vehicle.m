2bab4dcad51d4747e1eff93fa7dba1598f38d862
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-11 11:09:39 +0300

    Added Friction

--- Vehicle.m
+++ Vehicle.m
@@ -22,14 +22,15 @@ classdef Vehicle
             self.mul_a = (m + 4 * self.fan.I / (n * self.Z^2) / self.d^2)^ - 1;
             self.mul_q = 2 / (n * self.Z * self.d);
         end
-        function [a, v_t, F_t, F_d, F_q, F_net] = Acceleration(self, v)
+        function [a, v_t, F_t, F_d, F_q, F_f, F_net] = Acceleration(self, v)
             v_t = self.air.TrueSpeed(v);
             w_b = 2 / self.d * v;
             w_p = w_b / self.Z;
             [F_t, T_q] = self.fan.Find(self.air, v_t, w_p);
             F_d = self.F_d(v_t);
             F_q = self.mul_q * T_q;
-            F_net = F_t - F_d - F_q;
+            F_f = 0.04905 + v / (20/3600e-3) * (0.0614214);
+            F_net = F_t - F_d - F_q - F_f;
             a = F_net * self.mul_a;
         end
         function [v_min, v_max] = SpeedBoundary(self)
