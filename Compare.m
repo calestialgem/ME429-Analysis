0608cd26645221e80a39336831b37734151f4585
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-12 21:24:45 +0300

    Improved Comparison

--- Compare.m
+++ Compare.m
@@ -27,16 +27,29 @@ v_real = 5.64;
 v_initial = Simulate(initial, t_f, false);
 v_revised = Simulate(revised, t_f, false);
 
+v_top_initial = TopSpeed(initial.vehicle(1));
+v_top_revised = TopSpeed(revised.vehicle(1));
+
 e_initial = (v_initial - v_real) / v_real;
 e_revised = (v_revised - v_real) / v_real;
 
 fprintf(fileID, "Design:\n");
-fprintf(fileID, "* d = %.0f mm\n", d * 1e3);
-fprintf(fileID, "* Z = %.2f\n", Z);
-fprintf(fileID, "\nAt t = %.2f s\n", t_f);
-fprintf(fileID, "\n- v_real    = %10.2f m/s\n", v_real);
-fprintf(fileID, "\n- v_initial = %10.2f m/s ; Error = %5.2f%%\n", v_initial, e_initial * 100);
-fprintf(fileID, "\n- v_revised = %10.2f m/s ; Error = %5.2f%%\n", v_revised, e_revised * 100);
+fprintf(fileID, "d     = %5.2f mm\n", d * 1e3);
+fprintf(fileID, "Z     = %5.2f\n", Z);
+fprintf(fileID, "t_f   = %5.2f s\n", t_f);
+fprintf(fileID, "v_f   = %5.2f m/s\n", v_real);
+
+fprintf(fileID, "\nInitial Analysis:\n");
+fprintf(fileID, "v_f   = %5.2f m/s\n", v_initial);
+fprintf(fileID, "e     = %5.2f %%\n", e_initial * 100);
+fprintf(fileID, "v_top = %5.2f m/s\n", v_top_initial);
+fprintf(fileID, "rel   = %5.2f\n", v_top_initial / air.V);
+
+fprintf(fileID, "\nRevised Analysis:\n");
+fprintf(fileID, "v_f   = %5.2f m/s\n", v_revised);
+fprintf(fileID, "e     = %5.2f %%\n", e_revised * 100);
+fprintf(fileID, "v_top = %5.2f m/s\n", v_top_revised);
+fprintf(fileID, "rel   = %5.2f\n", v_top_revised / air.V);
 
 if fclose(fileID) ~= 0
     fprintf('Error while closing the file %s!\n', fileName);
